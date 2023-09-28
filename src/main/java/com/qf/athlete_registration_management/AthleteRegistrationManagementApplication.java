32114e504a35fd1f126a8368f01763ea93c181d0
仇国轩 <783919546@qq.com>
2023-09-28 09:43:12 +0800

    创建测试提交远程仓库

---
+++ src/main/java/com/qf/athlete_registration_management/AthleteRegistrationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.qf.athlete_registration_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteRegistrationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteRegistrationManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qf/athlete_registration_management/AthleteRegistrationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.athlete_registration_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleteRegistrationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
