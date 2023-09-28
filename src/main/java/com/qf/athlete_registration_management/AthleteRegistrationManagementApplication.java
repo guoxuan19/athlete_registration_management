ae4d5701641df676160eae21ad85be473a25dccf
仇国轩 <783919546@qq.com>
2023-09-28 09:47:55 +0800

    修改

--- src/main/java/com/qf/athlete_registration_management/AthleteRegistrationManagementApplication.java
+++ src/main/java/com/qf/athlete_registration_management/AthleteRegistrationManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AthleteRegistrationManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AthleteRegistrationManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AthleteRegistrationManagementApplication.class, args);
+    }
 
 }
